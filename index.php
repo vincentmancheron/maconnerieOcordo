3f2325fffc6088464c70d2507ed8c6963c3c7b33
Vincent Mancheron <vmancheron@yahoo.fr>
2021-06-30 12:15:39 +0200

    30/06/21 12:15

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+
+include(dirname(__FILE__).'/views/templates/header.php)');
+
+include(dirname(__FILE__).'/views/templates/footer.php)');
---
+++ utils/regex.php
@@ -0,0 +1,5 @@
+<?php
+define('regAlpha','[a-zA-Zéèçàùâêîûüëï ,\'"]*');
+define('regNumber','\d*');
+define('regPhoneNumber','(0|\+33)[1-9]( *[0-9]{2}){4}');
+define('regEmail','[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}');
