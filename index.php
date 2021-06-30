9b8ded5d529ba6584b9d82362fe1cb5eb6f75716
GuillaumeDavid23 <guillaume.david744@orange.fr>
2021-06-30 16:28:10 +0200

    test

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-include('views/templates/header.php');
+include(dirname(__FILE__).'/views/templates/header.php');
 
 $website = simplexml_load_file('models/source.xml');
-switch ($_GET['action']) {
+
+if(isset($_GET['action'])){
+    switch ($_GET['action']) {
     case '':
         echo $website->page[0]->content;
         break;
@@ -17,7 +19,11 @@ switch ($_GET['action']) {
         break;
     case 3:
         echo $website->page[3]->content;
+    }
+}else{
+    echo $website->page[0]->content;
 }
+
 
 include('views/templates/footer.php');
 
