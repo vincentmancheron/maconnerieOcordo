f37340bb6082de5860a326fa4287991ddcd3d275
Vincent Mancheron <vmancheron@yahoo.fr>
2021-06-30 16:22:12 +0200

    30/06/21 16:22

--- index.php
+++ index.php
@@ -1,24 +1,7 @@
 <?php
 include('views/templates/header.php');
 
-$website = simplexml_load_file('models/source.xml');
-switch ($_GET['action']) {
-    case '':
-        echo $website->page[0]->content;
-        break;
-    case 0:
-        echo $website->page[0]->content;
-        break;
-    case 1:
-        echo $website->page[1]->content;
-        break;
-    case 2:
-        echo $website->page[2]->content;
-        break;
-    case 3:
-        echo $website->page[3]->content;
-}
+include('controllers/xmlIncluder-ctrl.php');
 
 include('views/templates/footer.php');
-
 ?>
