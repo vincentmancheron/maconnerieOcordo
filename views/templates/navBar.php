358aee1bf18ff87ffbe9bf7d361fd78a0bb4007a
Vincent Mancheron <vmancheron@yahoo.fr>
2021-07-01 11:23:51 +0200

    01/07/21 11:23

--- views/templates/navBar.php
+++ views/templates/navBar.php
@@ -4,6 +4,7 @@
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <img class="d-lg-none mx-auto" src="/assets/img/logo.png" height="50" width="50">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mx-auto w-75 justify-content-around">
                 <li class="nav-item">
@@ -12,7 +13,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="index.php?action=1">Qui sommes nous</a>
                 </li>
-                <div><img src="/assets/img/logo.png" height="50" width="50"></div>
+                <div><img class="d-none d-lg-block" src="/assets/img/logo.png" height="50" width="50"></div>
                 <li class="nav-item">
                     <a class="nav-link" href="index.php?action=2">Nos clients témoignent</a>
                 </li>
